e11e04ce654e9be764f2547276558faf9009b158
Artur Grabowski <art@blahonga.org>
2015-04-20 14:19:10 +0200

    go fmt.

--- jsm_test.go
+++ jsm_test.go
@@ -2,8 +2,8 @@ package jsm_test
 
 import (
 	"encoding/json"
-	"testing"
 	"fmt"
+	"testing"
 )
 
 /*
@@ -22,41 +22,42 @@ import (
 // The JSON has no indentation so that we can easily compare
 // that what got encoded it the same thing that got decoded
 // earlier.
-const t1test =`{"foo":"bar","t":{"t1":{"a":1,"b":2}}}`
+const t1test = `{"foo":"bar","t":{"t1":{"a":1,"b":2}}}`
 const t2test = `{"foo":"bar","t":{"t2":{"c":1,"d":"str"}}}`
-
 
 type tface interface {
 	Tface() int
 }
 
 type t1 struct {
-	A int	`json:"a"`
-	B int	`json:"b"`
+	A int `json:"a"`
+	B int `json:"b"`
 }
-func (t *t1)Tface() int {
+
+func (t *t1) Tface() int {
 	return t.A + t.B
 }
 
 type t2 struct {
-	C int		`json:"c"`
-	D string	`json:"d"`
+	C int    `json:"c"`
+	D string `json:"d"`
 }
-func (t *t2)Tface() int {
+
+func (t *t2) Tface() int {
 	return t.C + len(t.D)
 }
 
 type xx struct {
 	Foo string `json:"foo"`
-	T tface `json:"t"`
+	T   tface  `json:"t"`
 }
 
 type xxJSON struct {
-	Foo string `json:"foo"`
-	T map[string]*json.RawMessage `json:"t"`	
+	Foo string                      `json:"foo"`
+	T   map[string]*json.RawMessage `json:"t"`
 }
 
-func (x *xx)UnmarshalJSON(data []byte) error {
+func (x *xx) UnmarshalJSON(data []byte) error {
 	var xd xxJSON
 
 	if err := json.Unmarshal(data, &xd); err != nil {
@@ -71,16 +72,19 @@ func (x *xx)UnmarshalJSON(data []byte) error {
 	}
 	for t, v := range xd.T {
 		switch t {
-		case "t1":	x.T = &t1{}
-		case "t2":	x.T = &t2{}
-		default:	return fmt.Errorf("xx.UnmarshalJSON: unknown t type: %v", xd.T)
+		case "t1":
+			x.T = &t1{}
+		case "t2":
+			x.T = &t2{}
+		default:
+			return fmt.Errorf("xx.UnmarshalJSON: unknown t type: %v", xd.T)
 		}
 		return json.Unmarshal(*v, x.T)
 	}
 	return nil
 }
 
-func (x *xx)MarshalJSON() ([]byte, error) {
+func (x *xx) MarshalJSON() ([]byte, error) {
 	var xd xxJSON
 
 	xd.Foo = x.Foo
@@ -95,9 +99,12 @@ func (x *xx)MarshalJSON() ([]byte, error) {
 	jr := json.RawMessage(j)
 
 	switch x.T.(type) {
-	case *t1:	xd.T["t1"] = &jr
-	case *t2:	xd.T["t2"] = &jr
-	default:	return nil, fmt.Errorf("xx.MarshalJSON: unknown t type")
+	case *t1:
+		xd.T["t1"] = &jr
+	case *t2:
+		xd.T["t2"] = &jr
+	default:
+		return nil, fmt.Errorf("xx.MarshalJSON: unknown t type")
 	}
 	return json.Marshal(&xd)
 }
@@ -109,11 +116,11 @@ func (x *xx)MarshalJSON() ([]byte, error) {
 // as we want. This was slightly confusing.
 func TestRawMessage(t *testing.T) {
 	msg := `{"foo":"bar"}`
-	var m json.RawMessage			
+	var m json.RawMessage
 	if err := json.Unmarshal([]byte(msg), &m); err != nil {
 		t.Fatal(err)
 	}
-	msg2, err := json.Marshal(&m)		 // if this isn't &m, we're screwed.
+	msg2, err := json.Marshal(&m) // if this isn't &m, we're screwed.
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -167,7 +174,7 @@ func TestEnc1(t *testing.T) {
 	}
 	s := string(bs)
 	if s != t1test {
-		t.Errorf("encoding '%s' != original '%s'", s, t1test);
+		t.Errorf("encoding '%s' != original '%s'", s, t1test)
 	}
 }
 
@@ -182,6 +189,6 @@ func TestEnc2(t *testing.T) {
 	}
 	s := string(bs)
 	if s != t2test {
-		t.Errorf("encoding '%s' != original '%s'", s, t2test);
+		t.Errorf("encoding '%s' != original '%s'", s, t2test)
 	}
 }
